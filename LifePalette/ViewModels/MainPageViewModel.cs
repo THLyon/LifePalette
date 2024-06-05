e69adf8188d5a0894f42b4b7af828de1273585c8
Tanner Lyon <tlyon@iwerk.com>
2024-06-05 13:25:58 -0400

    updated basic structure in views, viewmodels, models

---
+++ LifePalette/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Microsoft.Maui.Controls; 
+
+namespace LifePalette.ViewModels
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        private string _greeting;
+
+        public string Greeting
+        {
+            get => _greeting;
+            set
+            {
+                if (_greeting != value)
+                {
+                    _greeting = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public ICommand LoadGreetingCommand { get; }
+
+        public MainPageViewModel()
+        {
+            LoadGreetingCommand = new Command(OnLoadGreeting);
+        }
+
+        private void OnLoadGreeting()
+        {
+            Greeting = "Welcome to LifePalette!";
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
