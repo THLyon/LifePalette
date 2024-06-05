6eefa65aeda41ccbfca421640a4b181c0c2829ee
Tanner Lyon <tlyon@iwerk.com>
2024-06-05 11:11:21 -0400

    Initial commit

---
+++ LifePalette/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace LifePalette
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ LifePalette/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LifePalette
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
